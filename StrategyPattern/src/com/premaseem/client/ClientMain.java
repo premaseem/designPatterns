ea2c76393e45579339e46e0a1225b69c7c251aa0
PremAseem Jain <premaseem.webmaster@gmail.com>
2014-08-29 00:59:56 +0530

    Strategy Design Pattern Project folder added

---
+++ StrategyPattern/src/com/premaseem/client/ClientMain.java
@@ -0,0 +1,24 @@
+package com.premaseem.client;
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+		System.out.println("This is the Client Main file for Strategy Pattern");
+		System.out.println("Client has several Stragegy which can be consumed at run time ");
+		System.out.println("Please input 2 number and the stragatic mathematical operation code you want to add ");
+		System.out.println("Please enter first number ");
+		System.out.println("Pleaes enter second number");
+		System.out.println("ADD");
+		System.out.println("SUB");
+		System.out.println("MUL");
+		System.out.println("Please enter opration code, on the run time strategy would be picked based on your choice of operation");
+		
+		System.out.println("You picked stragety as ");
+		System.out.println("Final result is ");
+		
+		System.out.println("Thanks by Prem Aseem ");
+
+		
+	}
+
+}
