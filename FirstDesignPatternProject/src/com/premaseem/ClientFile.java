fe96cecb705f75077117f92505a1c88fa76f7709
PremAseem Jain <premaseem.webmaster@gmail.com>
2014-08-29 00:44:05 +0530

    Very first checkin of this repository

---
+++ FirstDesignPatternProject/src/com/premaseem/ClientFile.java
@@ -0,0 +1,9 @@
+package com.premaseem;
+
+public class ClientFile {
+
+	public static void main(String... args){
+		System.out.println("This is very first file for Design patterns project");
+		System.out.println("Prem aseem welcomes you");
+	}
+}
