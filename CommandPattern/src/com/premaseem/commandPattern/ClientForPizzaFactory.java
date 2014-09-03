8e9aa74efee43099cd96733ff62c716faf16cf28
PremAseem Jain <premaseem.webmaster@gmail.com>
2014-09-04 01:50:26 +0530

    Command Pattern - Project folder is checked in for Remote control example

---
+++ CommandPattern/src/com/premaseem/commandPattern/ClientForPizzaFactory.java
@@ -0,0 +1,33 @@
+package com.premaseem.commandPattern;
+
+import java.util.Scanner;
+
+public class ClientForPizzaFactory {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int repeatRunFlag = 1;
+		while (repeatRunFlag == 1) {
+
+			System.out.println("This is the Client Main Command Pattern with Remote control Example ");
+			
+			System.out.println("Loading the remote with approviate commands and mapping them in slots ... ");
+			
+			System.out.println("Please press the any command between 0 to 10 for operation ");
+			System.out.println("Remember 0 is for UNDO and 10 is celebration mode ");
+
+			int remoteButtonNumber = scan.nextInt();
+
+
+			// PizzaBase pizza = PizzaFactory.getPizza(pizzaType);
+
+			System.out.println("\n $$$$$$$$$$$$$$$$$$$$  Thanks by Prem Aseem $$$$$$$$$$$$$$$$$$$$$$ \n ");
+			System.out.println("Do you want to Re-run this program - Press 1 for yes and 0 or other digits to EXIT ");
+			try {
+				repeatRunFlag = scan.nextInt();
+			} catch (Exception e) {
+				repeatRunFlag = 0;
+			}
+		}
+	}
+}
