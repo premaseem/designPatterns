84bc21b8b8bb123e29753c35ddd8ef232340a004
PremAseem Jain <premaseem.webmaster@gmail.com>
2014-09-17 03:19:34 +0530

    Proxy Pattern - Example of Proxy doctor checked in

---
+++ ProxyPatternProxyDoctorExample/src/com/premaseem/proxyDoctor/HospitalService.java
@@ -0,0 +1,13 @@
+package com.premaseem.proxyDoctor;
+
+public class HospitalService {
+
+	public void callNurse() {
+		System.out.println("Doing the basic check up and like BP, breathing etc");
+	}
+
+	public void generateSummaryRepor(){
+		System.out.println("Call pathology and generate reports ");
+	}
+	
+}
