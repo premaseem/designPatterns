f90b5c4a7b7f478029d80f1fcfe2195d830f26f8
PremAseem Jain <premaseem.webmaster@gmail.com>
2014-09-18 00:18:37 +0530

    Proxy Pattern - Example of Role based access to Employee System (Projectrive Proxy) Manager can set rating of employee but cannot see salary Employee can see salary but cannot set it

---
+++ ProxyPatternRoleBasedAccessOfEmployee/src/com/premaseem/protectiveProxy/EmployeeInterface.java
@@ -0,0 +1,13 @@
+package com.premaseem.protectiveProxy;
+
+public interface EmployeeInterface {
+
+
+	String getName();
+	void setName(String name);
+	int getRating() ;
+	void setRating(int rating);
+	int getSalary();
+	void setSalary(int salary);
+	
+}
